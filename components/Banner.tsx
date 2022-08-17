5d11da034f3e90524dc3555dc166b583f6379821
Kamfeskaya <tatendachris@gmail.com>
2022-08-17 07:42:20 +0200

    feat: add random labels

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,6 +1,42 @@
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
+const roles = [
+  'Halfstack Developer',
+  'React Developer',
+  'Frontend Developer',
+  'Glorified Typist',
+  'Random Person',
+];
+const titles = [
+  'Blockchain Engineer',
+  'UI/UX Designer',
+  'Motorsport Legend',
+  'Software Engineer',
+  'Pencil Artist',
+  'Great Person',
+];
+const locations = [
+  'Harare, Zimbabwe',
+  'Gweru, Zimbabwe',
+  'Remote, Earth',
+  'Earth, Milky Way Galaxy',
+];
 export default function Banner() {
+  const [options] = useState({
+    role: roles[Math.floor(Math.random() * roles.length)],
+    title: titles[Math.floor(Math.random() * titles.length)],
+    location: locations[Math.floor(Math.random() * locations.length)],
+  });
+
+  useEffect(() => {
+    // Randomly pick one of the possible combinations
+    /*setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * allCombinations.length);
+      setOptions(allCombinations[randomIndex]);
+    }, 5000);*/
+  }, []);
+
   return (
     <div className="flex flex-col justify-center py-12">
       <div className="py-8">
@@ -14,13 +50,13 @@ export default function Banner() {
       <h1 className="text-3xl subpixel-antialiased font-medium tracking-tighter text-gray-800 dark:text-gray-200 lg:text-5xl">
         Hey there! I&#39;m Tatenda, a{' '}
         <span className="text-orange-600 dark:text-orange-400">
-          Halfstack Developer
+          {` ${options?.role} `}
         </span>{' '}
-        +{' '}
+        +
         <span className="text-red-500 dark:text-red-400">
-          Blockchain Engineer{' '}
+          {` ${options?.title} `}
         </span>
-        based in Harare, Zimbabwe
+        based in {` ${options?.location}`}.
       </h1>
       <p className="py-4 mt-8 text-xl">
         I help startups and developers{' '}
---
+++ packages/artworker/index.ts
@@ -0,0 +1,3 @@
+import Artworker from './app';
+
+export { Artworker };
